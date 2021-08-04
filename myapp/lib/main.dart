dbf4d057403d7b25b7874caa9b6c5cd0fa4ac35d
Rebecca Zee <ryz283@gmail.com>
2021-08-04 17:15:49 -0400

    AndroidX Migration

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,32 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Home(),
-));
+void main() => runApp(const Home());
 
 class Home extends StatelessWidget {
+  const Home({
+    Key? key,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: Text('my first app'),
-          centerTitle: true,
-          backgroundColor: Colors.red[600]
-      ),
-      body: Center(
-        child: IconButton(
-          onPressed: () {
-            print('you clicked me');
-          },
-          icon: Icon(Icons.alternate_email),
-          color: Colors.amber,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.red[600],
-        child: Text('click'),
-      ),
-    );
+    return MaterialApp(
+        title: 'my first app',
+        home: Scaffold(
+          appBar: AppBar(
+              title: const Text('my first app'),
+              centerTitle: true,
+              backgroundColor: Colors.red[600]),
+          body: Center(
+            child: IconButton(
+              onPressed: () {
+                print('you clicked me');
+              },
+              icon: const Icon(Icons.alternate_email),
+              color: Colors.amber,
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+              backgroundColor: Colors.red[600],
+              child: const Text('click'),
+              onPressed: () {}),
+        ));
   }
 }
 
